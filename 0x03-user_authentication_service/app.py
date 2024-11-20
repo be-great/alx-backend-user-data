b6d92c2cfe1a975d6d2dbe7ad6fc5e6baaf8b8d1
be-great <begreat5333@gmail.com>
2024-11-20 23:23:26 +0300

    update task 1

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """6. Basic Flask app"""
-# from auth import Auth
+from auth import Auth
 from flask import (Flask,
                    jsonify,
                    request,
@@ -8,7 +8,7 @@ from flask import (Flask,
                    redirect)
 
 app = Flask(__name__)
-# AUTH = Auth()
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -16,12 +16,23 @@ def helloWorld() -> str:
     """hello world"""
     msg = {"message": "Bienvenue"}
     return jsonify(msg)
-# @app.route('/', methods=['GET'])
-# def hello_world() -> str:
-#     """ Base route for authentication service API """
-#     msg = {"message": "Bienvenue"}
-#     return jsonify(msg)
 
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """register a user"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)    
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+         return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
 
 # @app.route('/users', methods=['POST'])
 # def register_user() -> str:
