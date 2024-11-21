c609481e20463420d3763426383b3d676ceda485
be-great <begreat5333@gmail.com>
2024-11-22 00:41:43 +0300

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,6 +72,15 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """take a session id and return a string or none"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
 #     def create_session(self, email: str) -> str:
 #         """ Returns session ID for a user """
 #         try:
@@ -115,15 +124,6 @@ class Auth:
 #         self._db.update_user(user.id, session_id=session_id)
 
 #         return session_id
-def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
-    """take a session id and return a string or none"""
-    if session_id is None:
-        return None
-    try:
-        user = self._db.find_user_by(session_id=session_id)
-    except NoResultFound:
-        return None
-    return user
 #         try:
 #             user = self._db.find_user_by(session_id=session_id)
 #         except NoResultFound:
